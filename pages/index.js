1b351fb30c974bac8a19d0294686daee836105e4
David Lemvigh <dle@siteimprove.com>
2021-06-18 17:17:46 +0200

    A quick hook

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useState, useCallback } from "react"
 
 export default function Home() {
   console.log("hello ye who looks in consoles");
+  const [count, setCount] = useState(0)
+  const increment = useCallback(() => setCount(x => x + 1), []);
+  const decrement = useCallback(() => setCount(x => Math.max(0, x - 1)), []);
+  
   return (
     <div className="container">
       <Head>
@@ -16,6 +21,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <div>
+          count: ({count}) <button onClick={increment}>+++</button><button onClick={decrement}>---</button>
+        </div>
+    
       </main>
 
       <Footer />
